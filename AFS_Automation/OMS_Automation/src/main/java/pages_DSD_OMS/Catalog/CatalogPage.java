d3cb96eb0b0f7fabc21d4ddcab2550c52d4d53b2
Divya Ramadas <Divya.Ramadas@afsi.com>
2023-07-26 10:09:44 -0400

    Changes in runner 26th july Divya

--- AFS_Automation/OMS_Automation/src/main/java/pages_DSD_OMS/Catalog/CatalogPage.java
+++ AFS_Automation/OMS_Automation/src/main/java/pages_DSD_OMS/Catalog/CatalogPage.java
@@ -1,9 +1,5 @@
 package pages_DSD_OMS.Catalog;
 
-<<<<<<< HEAD
-=======
-//import com.sun.org.apache.xml.internal.security.c14n.implementations.Canonicalizer20010315ExclWithComments;
->>>>>>> cfd851261460b93c4e6039073ff046846beedf5f
 import helper.HelpersMethod;
 import io.cucumber.java.Scenario;
 import org.openqa.selenium.*;
@@ -1281,6 +1277,7 @@ public class CatalogPage
         try
         {
             WebElement listInput=HelpersMethod.FindByElement(driver,"xpath","//input[contains(@id,'catalog-list-view-quantity-input')]");
+            HelpersMethod.ScrollElement(driver,listInput);
             HelpersMethod.ActSendKey(driver,listInput,80,s);
         }
         catch (Exception e){}
--- AFS_Automation/OMS_Automation/src/test/java/testRunner_DSD_OMS/DSDEnv/TestRunnerOE2.java
+++ AFS_Automation/OMS_Automation/src/test/java/testRunner_DSD_OMS/DSDEnv/TestRunnerOE2.java
@@ -2,10 +2,7 @@ package testRunner_DSD_OMS.DSDEnv;
 
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.Optional;
-import org.testng.annotations.Parameters;
+import org.testng.annotations.*;
 import util.MailSend;
 import util.TestBase;
 
@@ -30,7 +27,7 @@ import java.io.IOException;
 public class TestRunnerOE2 extends AbstractTestNGCucumberTests
 {
         /* Created by Divya.Ramadas@afsi.com */
-        @Parameters({"environment"})
+  /*      @Parameters({"environment"})
         @BeforeClass
         public static void beforeClass(@Optional String envi)
         {
@@ -49,6 +46,34 @@ public class TestRunnerOE2 extends AbstractTestNGCucumberTests
                 Thread.sleep(10000);
                 //MailSend.sendMail();
                 TestBase.CloseBrowser();
+                if(TestBase.testEnvironment.get_browser().equalsIgnoreCase("Firefox"))
+                {
+                        Runtime.getRuntime().exec("taskkill /F /IM geckodriver.exe");
+                }
+                else
+                if(TestBase.testEnvironment.get_browser().equalsIgnoreCase("Chrome"))
+                {
+                        Runtime.getRuntime().exec("taskkill /F /IM chromedriver.exe");
+                }
+        }*/
+        @Parameters({"environment"})
+        @BeforeMethod
+        public static void beforeClass(@Optional("environment") String envi)
+        {
+                try
+                {
+                        TestBase.InitializeProp(envi);
+                        TestBase.SetDriver(TestBase.testEnvironment.get_browser());
+                }
+                catch (Exception e) { }
+        }
+
+        @AfterMethod
+        public static void afterclass() throws InterruptedException, MessagingException, IOException
+        {
+                Thread.sleep(50000);
+                TestBase.CloseBrowser();
+
                 if(TestBase.testEnvironment.get_browser().equalsIgnoreCase("Firefox"))
                 {
                         Runtime.getRuntime().exec("taskkill /F /IM geckodriver.exe");
